6e45b97993312cb2a53da5a047580bf8c304731d
Emanuel Boaventura <emanuelbmatos321@gmail.com>
2023-08-12 14:56:37 -0400

    feat: make carousel animations

--- src/pages/components/ImageCarousel.tsx
+++ src/pages/components/ImageCarousel.tsx
@@ -1,67 +1,64 @@
-import Image from 'next/image'
-import React, { useState } from 'react'
-import cookie1 from '@/public/cookie-1.jpg'
-import cookie2 from '@/public/cookie-2.jpg'
-import cookie3 from '@/public/cookie-3.jpg'
 import { CaretLeft, CaretRight } from '@phosphor-icons/react'
+import Image, { StaticImageData } from 'next/image'
+import { useState } from 'react'
 
-const images = [
-  {
-    src: cookie1,
-    description:
-      'Biscoitos caseiros irresistíveis! Temos uma ampla variedade de sabores para todos os paladares. E melhor de tudo, são feitos com amor. Experimente essa delícia única!',
-  },
-  {
-    src: cookie2,
-    description:
-      'Explore a mistura perfeita de texturas e sabores em nossos biscoitos de aveia e chocolate. Cada mordida combina crocância com indulgência de cacau, criando uma experiência única.',
-  },
-  {
-    src: cookie3,
-    description:
-      'Experimente o sabor rico da manteiga em nossos biscoitos artesanais. Com uma casquinha crocante e interior macio, cada mordida oferece indulgência e conforto como nenhum outro.',
-  },
-]
+interface IImageCarousel {
+  images: {
+    src: StaticImageData
+    description: string
+  }[]
+}
 
-const ImageCarousel = () => {
-  const [currentImage, setCurrentImage] = useState(0)
+const ImageCarousel = ({ images }: IImageCarousel) => {
+  const [current, setCurrent] = useState(0)
 
   const nextImage = () => {
-    setCurrentImage((currentImage + 1) % images.length)
+    setCurrent(prevState =>
+      prevState === images.length - 1 ? 0 : prevState + 1
+    )
   }
 
   const prevImage = () => {
-    setCurrentImage((currentImage - 1 + images.length) % images.length)
+    setCurrent(prevState =>
+      prevState === 0 ? images.length - 1 : prevState - 1
+    )
   }
 
   return (
-    <div className='relative grid grid-cols-[240px_1fr] justify-between w-[46.5rem] gap-8 items-center'>
-      <div className='flex items-center w-60 h-72 justify-center border-white border-4 overflow-hidden'>
-        <Image
-          src={images[currentImage].src}
-          alt='Carousel'
-          className='w-full object-contain'
-        />
+    <div className='relative'>
+      <div className='max-w-[76rem] overflow-hidden relative'>
+        <div className='flex w-fit'>
+          {images.map(image => (
+            <div
+              key={image.description}
+              className={`grid grid-cols-[240px_1fr] justify-between w-[46.5rem] lg:w-[76rem] gap-8 items-center `}
+              style={{
+                transform: `translateX(-${current * 100}%)`,
+                transition: 'transform 0.3s ease-in-out',
+              }}
+            >
+              <div className='flex  items-center w-60 h-72 justify-center border-white border-4'>
+                <Image
+                  src={image.src}
+                  alt='Carousel'
+                  className='w-full object-contain'
+                />
+              </div>
+
+              <p>{image.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
-
-      <p>{images[currentImage].description}</p>
-
-      {/* <a
-        href='#form'
-        className='default-button !bg-default-500 !text-default-50 flex'
-      >
-        Pedir agora!
-      </a> */}
-
       <button
         onClick={prevImage}
-        className='text-white absolute top-1/2 -translate-y-1/2 -left-[2rem] no-animation'
+        className='text-white absolute top-1/2 -translate-y-1/2 -left-[3rem] no-animation'
       >
         <CaretLeft size={32} />
       </button>
       <button
         onClick={nextImage}
-        className='text-white absolute top-1/2 -translate-y-1/2 -right-[2rem] no-animation'
+        className='text-white absolute top-1/2 -translate-y-1/2 -right-[3rem] no-animation'
       >
         <CaretRight size={32} />
       </button>
--- src/pages/modules/Product.tsx
+++ src/pages/modules/Product.tsx
@@ -1,6 +1,28 @@
+import cookie1 from '@/public/cookie-1.jpg'
+import cookie2 from '@/public/cookie-2.jpg'
+import cookie3 from '@/public/cookie-3.jpg'
+import { useState } from 'react'
 import ImageCarousel from '../components/ImageCarousel'
 
 export default function Product() {
+  const [images, setImages] = useState([
+    {
+      src: cookie1,
+      description:
+        'Biscoitos caseiros irresistíveis! Temos uma ampla variedade de sabores para todos os paladares. E melhor de tudo, são feitos com amor. Experimente essa delícia única!',
+    },
+    {
+      src: cookie2,
+      description:
+        'Explore a mistura perfeita de texturas e sabores em nossos biscoitos de aveia e chocolate. Cada mordida combina crocância com indulgência de cacau, criando uma experiência única.',
+    },
+    {
+      src: cookie3,
+      description:
+        'Experimente o sabor rico da manteiga em nossos biscoitos artesanais. Com uma casquinha crocante e interior macio, cada mordida oferece indulgência e conforto como nenhum outro.',
+    },
+  ])
+
   return (
     <section className='flex bg-default-100 text-default-950'>
       <div className='max-container flex flex-col /sm:flex-row justify-between items-center py-10'>
@@ -9,7 +31,7 @@ export default function Product() {
         </h1>
 
         <div className='flex flex-col  items-center sm:items-start'>
-          <ImageCarousel />
+          <ImageCarousel images={images} />
         </div>
       </div>
     </section>
